b68c11b6a39784255fc29bb58f90d07d48ffd055
Ranjith K <ranjith.masthikatte@gmail.com>
2023-06-22 17:00:24 +0200

    Env variable

--- avighna-artefact-linux.sh
+++ avighna-artefact-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-root_dir=$(pwd)
+env ROOT_DIR=$(pwd)
 
 npm install -g node-gyp
 
@@ -24,45 +24,45 @@ apt-get update -y && apt-get install -y maven
 apt-get update -y && apt-get install -y graphviz
 
 # give permissions to jar files and create necessary directories
-cd "$root_dir"/ || exit
-chmod 755 "$root_dir"/avighna-agent-1.0.0.jar
-chmod 755 "$root_dir"/avighna-cmd-interface-1.0.0.jar
-chmod 755 "$root_dir"/avighna-merger-1.0-SNAPSHOT-jar-with-dependencies.jar
+cd "$ROOT_DIR"/ || exit
+chmod 755 "$ROOT_DIR"/avighna-agent-1.0.0.jar
+chmod 755 "$ROOT_DIR"/avighna-cmd-interface-1.0.0.jar
+chmod 755 "$ROOT_DIR"/avighna-merger-1.0-SNAPSHOT-jar-with-dependencies.jar
 mkdir Spring-Projects
 mkdir Reflection-Projects
 mkdir Guice-Projects
 
 # Start with Spring projects
-cd "$root_dir"/Spring-Projects || exit
+cd "$ROOT_DIR"/Spring-Projects || exit
 
 # Install Fredbet project
 git clone https://github.com/ranjithkris/fredbet.git
-cd "$root_dir"/Spring-Projects/fredbet/ || exit
+cd "$ROOT_DIR"/Spring-Projects/fredbet/ || exit
 git checkout -b tags_release_2.0.0 tags/release_2.0.0
 bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && sdk use java 8.0.352-librca && mvn clean install -DskipTests"
 
 # Install Spring Initializer project
-cd "$root_dir"/Spring-Projects || exit
+cd "$ROOT_DIR"/Spring-Projects || exit
 git clone https://github.com/ranjithkris/start.spring.io.git
-cd "$root_dir"/Spring-Projects/start.spring.io/ || exit
+cd "$ROOT_DIR"/Spring-Projects/start.spring.io/ || exit
 git checkout 6c0944ead1bfef3444a9e1a422b3f134de2b7f48
 bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && sdk use java 11.0.17-librca && mvn clean install -DskipTests"
 
 # Install Spring Zipkin project
-cd "$root_dir"/Spring-Projects || exit
+cd "$ROOT_DIR"/Spring-Projects || exit
 git clone https://github.com/ranjithkris/zipkin.git
-cd "$root_dir"/Spring-Projects/zipkin/ || exit
+cd "$ROOT_DIR"/Spring-Projects/zipkin/ || exit
 git checkout 8a4f4b9c9a5a3204d9663ecef39d687785369c9a
 bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && sdk use java 8.0.352-librca && mvn clean install -DskipTests"
 git clone https://github.com/ranjithkris/pyramid_zipkin-example.git
-cd "$root_dir"/Spring-Projects/zipkin/pyramid_zipkin-example/ || exit
+cd "$ROOT_DIR"/Spring-Projects/zipkin/pyramid_zipkin-example/ || exit
 pip install pyramid_zipkin -U
 python3 setup.py install
 
 # Install CGBench project
-cd "$root_dir"/Spring-Projects || exit
+cd "$ROOT_DIR"/Spring-Projects || exit
 git clone https://github.com/ranjithkris/CGBench.git
-cd "$root_dir"/Spring-Projects/CGBench/ || exit
+cd "$ROOT_DIR"/Spring-Projects/CGBench/ || exit
 git checkout origin/for-avighna
 # RUN git checkout 65820234b5b9a6f65bd69d78570a3caedee7f1a1
 rm -r guice-credentials/
@@ -72,34 +72,34 @@ rm -r onlineshop/
 rm -r teleforum/
 rm -r webgoat/
 bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && sdk use java 8.0.352-librca && python3 buildAll.py"
-mv guice/ "$root_dir"/Guice-Projects/
+mv guice/ "$ROOT_DIR"/Guice-Projects/
 
 # Install Spring Petclinic project
-cd "$root_dir"/Spring-Projects || exit
+cd "$ROOT_DIR"/Spring-Projects || exit
 git clone https://github.com/ranjithkris/spring-petclinic.git
-cd "$root_dir"/Spring-Projects/spring-petclinic/ || exit
+cd "$ROOT_DIR"/Spring-Projects/spring-petclinic/ || exit
 git checkout 0c1fa8e8e2744125cc4bee725fe2de6dd76d3a4f
 bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && sdk use java 11.0.17-librca && mvn clean install -DskipTests"
 
 # Install Streamflow project (Guice Framework)
-cd "$root_dir"/Guice-Projects || exit
+cd "$ROOT_DIR"/Guice-Projects || exit
 git clone https://github.com/ranjithkris/streamflow.git
-cd "$root_dir"/Guice-Projects/streamflow/ || exit
+cd "$ROOT_DIR"/Guice-Projects/streamflow/ || exit
 git checkout -b tags_0.12.0 tags/0.12.0
 bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && sdk use java 8.0.352-librca && mvn clean install -DskipTests"
 
 # Install Java Reflection Test cases project by Florian Kübler
-cd "$root_dir"/Reflection-Projects || exit
+cd "$ROOT_DIR"/Reflection-Projects || exit
 git clone https://github.com/ranjithkris/JavaReflectionTestCases.git
-cd "$root_dir"/Reflection-Projects/JavaReflectionTestCases/ || exit
+cd "$ROOT_DIR"/Reflection-Projects/JavaReflectionTestCases/ || exit
 bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && sdk use java 8.0.352-librca && mvn clean install -DskipTests"
 
 # Install Runner projects
 
 # Install avighna which is used by helper runner
-cd "$root_dir"/ || exit
+cd "$ROOT_DIR"/ || exit
 mvn install:install-file \
-  -Dfile="$root_dir"/avighna-merger-1.0-SNAPSHOT-jar-with-dependencies.jar \
+  -Dfile="$ROOT_DIR"/avighna-merger-1.0-SNAPSHOT-jar-with-dependencies.jar \
   -DgroupId=de.fraunhofer.iem \
   -DartifactId=avighna-merger \
   -Dversion=1.0-SNAPSHOT \
@@ -107,13 +107,13 @@ mvn install:install-file \
   -DgeneratePom=true
 
 # Install CGBenchRunner
-cd "$root_dir"/CGBenchRunner/ || exit
+cd "$ROOT_DIR"/CGBenchRunner/ || exit
 bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && sdk use java 8.0.352-librca && mvn clean install -DskipTests"
 
 # Install JavaReflectionTestCaseRunner
-cd "$root_dir"/JavaReflectionTestRunner/ || exit
+cd "$ROOT_DIR"/JavaReflectionTestRunner/ || exit
 bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && sdk use java 8.0.352-librca && mvn clean install -DskipTests"
 
 bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && sdk use java 8.0.352-librca"
 
-cd "$root_dir"/ || exit
+cd "$ROOT_DIR"/ || exit
