d1b3c710c3a26e281fc4063e59b0c83e3165be93
Ranjith K <ranjith.masthikatte@gmail.com>
2023-06-23 11:13:56 +0200

    Structured the project

--- avighna-artefact-linux.sh
+++ avighna-artefact-linux.sh
@@ -35,18 +35,18 @@ bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && yes | sdk install java 8.0.3
 apt-get update -y && apt-get install -y maven
 apt-get update -y && apt-get install -y graphviz
 
-mkdir "$ROOT_DIR"/Reflection-Projects/
+mkdir "$ROOT_DIR"/JarFiles/Reflection-Projects/
 
-cd "$ROOT_DIR"/Spring-Projects/zipkin/ || exit
+cd "$ROOT_DIR"/JarFiles/Spring-Projects/zipkin/ || exit
 git clone https://github.com/ranjithkris/pyramid_zipkin-example.git
-cd "$ROOT_DIR"/Spring-Projects/zipkin/pyramid_zipkin-example/ || exit
+cd "$ROOT_DIR"/JarFiles/Spring-Projects/zipkin/pyramid_zipkin-example/ || exit
 pip3 install pyramid_zipkin -U
 python3 setup.py install
 
 # Install CGBench project
-cd "$ROOT_DIR"/Spring-Projects || exit
+cd "$ROOT_DIR"/JarFiles/Spring-Projects || exit
 git clone https://github.com/ranjithkris/CGBench.git
-cd "$ROOT_DIR"/Spring-Projects/CGBench/ || exit
+cd "$ROOT_DIR"/JarFiles/Spring-Projects/CGBench/ || exit
 git checkout -b for-avighna origin/for-avighna
 # RUN git checkout 65820234b5b9a6f65bd69d78570a3caedee7f1a1
 rm -r guice-credentials/
@@ -59,15 +59,15 @@ rm -r guice/
 bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && sdk use java 8.0.352-librca && python3 buildAll.py"
 
 # Install Java Reflection Test cases project by Florian Kübler
-cd "$ROOT_DIR"/Reflection-Projects || exit
+cd "$ROOT_DIR"/JarFiles/Reflection-Projects || exit
 git clone https://github.com/ranjithkris/JavaReflectionTestCases.git
-cd "$ROOT_DIR"/Reflection-Projects/JavaReflectionTestCases/ || exit
+cd "$ROOT_DIR"/JarFiles/Reflection-Projects/JavaReflectionTestCases/ || exit
 bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && sdk use java 8.0.352-librca && mvn clean install -DskipTests"
 
 # Install avighna which is used by helper runner
 cd "$ROOT_DIR"/ || exit
 mvn install:install-file \
-  -Dfile="$ROOT_DIR"/avighna-merger-1.0-SNAPSHOT-jar-with-dependencies.jar \
+  -Dfile="$ROOT_DIR"/JarFiles/avighna-merger-1.0-SNAPSHOT-jar-with-dependencies.jar \
   -DgroupId=de.fraunhofer.iem \
   -DartifactId=avighna-merger \
   -Dversion=1.0-SNAPSHOT \
@@ -82,6 +82,7 @@ bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && sdk use java 8.0.352-librca 
 cd "$ROOT_DIR"/JavaReflectionTestRunner/ || exit
 bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && sdk use java 8.0.352-librca && mvn clean install -DskipTests"
 
+bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && sdk default java 8.0.352-librca"
 bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && sdk use java 8.0.352-librca"
 
 cd "$ROOT_DIR"/ || exit
--- avighna-artefact-macos.sh
+++ avighna-artefact-macos.sh
@@ -28,18 +28,18 @@ curl -s "https://get.sdkman.io" | bash
 bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && yes | sdk install java 11.0.17-librca"
 bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && yes | sdk install java 8.0.352-librca"
 
-mkdir "$ROOT_DIR"/Reflection-Projects/
+mkdir "$ROOT_DIR"/JarFiles/Reflection-Projects/
 
-cd "$ROOT_DIR"/Spring-Projects/zipkin/ || exit
+cd "$ROOT_DIR"/JarFiles/Spring-Projects/zipkin/ || exit
 git clone https://github.com/ranjithkris/pyramid_zipkin-example.git
-cd "$ROOT_DIR"/Spring-Projects/zipkin/pyramid_zipkin-example/ || exit
+cd "$ROOT_DIR"/JarFiles/Spring-Projects/zipkin/pyramid_zipkin-example/ || exit
 pip install pyramid_zipkin -U
 python3 setup.py install
 
 # Install CGBench project
-cd "$ROOT_DIR"/Spring-Projects || exit
+cd "$ROOT_DIR"/JarFiles/Spring-Projects || exit
 git clone https://github.com/ranjithkris/CGBench.git
-cd "$ROOT_DIR"/Spring-Projects/CGBench/ || exit
+cd "$ROOT_DIR"/JarFiles/Spring-Projects/CGBench/ || exit
 git checkout -b for-avighna origin/for-avighna
 rm -r guice-credentials/
 rm -r music-store/
@@ -51,15 +51,15 @@ rm -r guice/
 bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && sdk use java 8.0.352-librca && python3 buildAll.py"
 
 # Install Java Reflection Test cases project by Florian Kübler
-cd "$ROOT_DIR"/Reflection-Projects || exit
+cd "$ROOT_DIR"/JarFiles/Reflection-Projects || exit
 git clone https://github.com/ranjithkris/JavaReflectionTestCases.git
-cd "$ROOT_DIR"/Reflection-Projects/JavaReflectionTestCases/ || exit
+cd "$ROOT_DIR"/JarFiles/Reflection-Projects/JavaReflectionTestCases/ || exit
 bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && sdk use java 8.0.352-librca && mvn clean install -DskipTests"
 
 # Install avighna which is used by helper runner
 cd "$ROOT_DIR"/ || exit
 mvn install:install-file \
-  -Dfile="$ROOT_DIR"/avighna-merger-1.0-SNAPSHOT-jar-with-dependencies.jar \
+  -Dfile="$ROOT_DIR"/JarFiles/avighna-merger-1.0-SNAPSHOT-jar-with-dependencies.jar \
   -DgroupId=de.fraunhofer.iem \
   -DartifactId=avighna-merger \
   -Dversion=1.0-SNAPSHOT \
@@ -74,6 +74,7 @@ bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && sdk use java 8.0.352-librca 
 cd "$ROOT_DIR"/JavaReflectionTestRunner/ || exit
 bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && sdk use java 8.0.352-librca && mvn clean install -DskipTests"
 
+bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && sdk default java 8.0.352-librca"
 bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && sdk use java 8.0.352-librca"
 
 cd "$ROOT_DIR"/ || exit
